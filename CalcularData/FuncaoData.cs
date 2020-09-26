c8f9456f7c598bacb0cb1c9b9b24eea676951980
Marcio Coelho <marcio@caster.com.br>
2020-09-26 12:23:48 -0300

    Primeira versão em funcionamento

---
+++ CalcularData/FuncaoData.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace CalcularData
+{
+    class FuncaoData : InterfaceData
+    {
+        public string dia { get; set; }
+        public string mes { get; set; }
+        public string ano { get; set; }
+        public string hora { get; set; }
+        public string minutos { get; set; }
+        public string minutos_a_alterar { get; set; }
+        public string operacao { get; set; }
+
+        static int minutos_ano { get; set; }
+        static int minutos_dia { get; set; }
+        static int minutos_hora { get; set; }
+        static int total_datahora_em_minutos { get; set; }
+
+        static int quociente_divisao_ano { get; set; }
+        static int resto_divisao_ano { get; set; }
+        static int quociente_divisao_dia { get; set; }
+        static int resto_divisao_dia { get; set; }
+        static int quociente_divisao_hora { get; set; }
+        static int resto_divisao_hora { get; set; }
+        static int calcula_dia_do_mes { get; set; }
+        static int calcula_mes { get; set; }
+
+        const int anoMinutos = 527040;     // ajuste para considerar 366 dias no ano para funcionar para os últimos 2 dias do ano
+        const int diaMinutos = 1440;
+        const int horaMinutos = 60;
+
+        public string CalcularData(string data, char op, long valor)
+        {
+            dia = data.Split(new char[] { '/', ' ', ':' }, 5)[0];
+            mes = data.Split(new char[] { '/', ' ', ':' }, 5)[1];
+            ano = data.Split(new char[] { '/', ' ', ':' }, 5)[2];
+            hora = data.Split(new char[] { '/', ' ', ':' }, 5)[3];
+            minutos = data.Split(new char[] { '/', ' ', ':' }, 5)[4];
+
+            operacao = op.ToString();
+            valor = (valor < 0 ? valor * -1 : valor);
+
+            minutos_a_alterar = valor.ToString();
+
+            TotalMinutosConvertidopelaDataHora();
+
+            // retorna em formato data e hora
+            return String.Format("{0}/{1}/{2} {3}:{4}",
+                calcula_dia_do_mes.ToString().PadLeft(2, '0'),
+                calcula_mes.ToString().PadLeft(2, '0'),
+                quociente_divisao_ano.ToString().PadLeft(4, '0'),
+                quociente_divisao_hora.ToString().PadLeft(2, '0'),
+                resto_divisao_hora.ToString().PadLeft(2, '0'));
+        }
+
+        private void TotalMinutosConvertidopelaDataHora()
+        {
+            ConverterAnoemMinutos();
+
+            ConverterDiaemMinutos();
+
+            ConverterDataHoraemMinutos();
+
+            total_datahora_em_minutos = minutos_ano + minutos_dia + minutos_hora;
+
+            ConverterMinutosemAno();
+        }
+
+        private void ConverterAnoemMinutos()
+        {
+            minutos_ano = Convert.ToInt32(ano) * anoMinutos;
+        }
+
+        private void ConverterDiaemMinutos()
+        {
+            int diaMes;
+
+            switch (mes)
+            {
+                case "01":
+                    diaMes = 0;
+                    break;
+                case "02":
+                    diaMes = 31;
+                    break;
+                case "03":
+                    diaMes = 59;
+                    break;
+                case "04":
+                    diaMes = 90;
+                    break;
+                case "05":
+                    diaMes = 120;
+                    break;
+                case "06":
+                    diaMes = 151;
+                    break;
+                case "07":
+                    diaMes = 181;
+                    break;
+                case "08":
+                    diaMes = 212;
+                    break;
+                case "09":
+                    diaMes = 243;
+                    break;
+                case "10":
+                    diaMes = 273;
+                    break;
+                case "11":
+                    diaMes = 304;
+                    break;
+                case "12":
+                    diaMes = 334;
+                    break;
+                default:
+                    diaMes = 0;
+                    break;
+            }
+
+            minutos_dia = diaMes * diaMinutos + Convert.ToInt32(dia) * diaMinutos;
+
+        }
+
+        private void ConverterDataHoraemMinutos()
+        {
+            minutos_hora = Convert.ToInt32(hora) * horaMinutos + Convert.ToInt32(minutos)
+                + Convert.ToInt32(minutos_a_alterar) * (operacao == "+" ? 1 : -1);
+        }
+
+        private void ConverterMinutosemAno()
+        {
+            quociente_divisao_ano = total_datahora_em_minutos / anoMinutos;
+            resto_divisao_ano = total_datahora_em_minutos % anoMinutos;
+            ConverterMinutosemDia();
+        }
+        private void ConverterMinutosemDia()
+        {
+            quociente_divisao_dia = resto_divisao_ano / diaMinutos;
+            resto_divisao_dia = resto_divisao_ano % diaMinutos;
+
+            if (quociente_divisao_dia == 0) quociente_divisao_dia = 1;
+
+            if (quociente_divisao_dia <= 31) { calcula_mes = 1; calcula_dia_do_mes = quociente_divisao_dia; }
+            if (quociente_divisao_dia >= 32 && quociente_divisao_dia < 60) { calcula_mes = 2; calcula_dia_do_mes = quociente_divisao_dia - 32 + 1; }
+            if (quociente_divisao_dia >= 60 && quociente_divisao_dia < 91) { calcula_mes = 3; calcula_dia_do_mes = quociente_divisao_dia - 60 + 1; }
+            if (quociente_divisao_dia >= 91 && quociente_divisao_dia < 121) { calcula_mes = 4; calcula_dia_do_mes = quociente_divisao_dia - 91 + 1; }
+            if (quociente_divisao_dia >= 121 && quociente_divisao_dia < 152) { calcula_mes = 5; calcula_dia_do_mes = quociente_divisao_dia - 121 + 1; }
+            if (quociente_divisao_dia >= 152 && quociente_divisao_dia < 182) { calcula_mes = 6; calcula_dia_do_mes = quociente_divisao_dia - 152 + 1; }
+            if (quociente_divisao_dia >= 182 && quociente_divisao_dia < 213) { calcula_mes = 7; calcula_dia_do_mes = quociente_divisao_dia - 182 + 1; }
+            if (quociente_divisao_dia >= 213 && quociente_divisao_dia < 244) { calcula_mes = 8; calcula_dia_do_mes = quociente_divisao_dia - 213 + 1; }
+            if (quociente_divisao_dia >= 244 && quociente_divisao_dia < 274) { calcula_mes = 9; calcula_dia_do_mes = quociente_divisao_dia - 244 + 1; }
+            if (quociente_divisao_dia >= 274 && quociente_divisao_dia < 305) { calcula_mes = 10; calcula_dia_do_mes = quociente_divisao_dia - 274 + 1; }
+            if (quociente_divisao_dia >= 305 && quociente_divisao_dia < 335) { calcula_mes = 11; calcula_dia_do_mes = quociente_divisao_dia - 305 + 1; }
+            if (quociente_divisao_dia >= 335 && quociente_divisao_dia <= 365) { calcula_mes = 12; calcula_dia_do_mes = quociente_divisao_dia - 335 + 1; }
+
+            ConverterMinutosemHora();
+        }
+        private void ConverterMinutosemHora()
+        {
+            quociente_divisao_hora = resto_divisao_dia / horaMinutos;
+            resto_divisao_hora = resto_divisao_dia % horaMinutos;
+        }
+    }
+
+}
---
+++ CalcularData/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string data;
+                char operacao;
+                long valor;
+
+                // Pede ao usuario que digite a data
+                Console.WriteLine("Digite a data e hora no formato dd/MM/yyyy HH:mm e pressione Enter");
+                data = Convert.ToString(Console.ReadLine());
+
+                // Pede ao usuario que digite a operação
+                Console.WriteLine("Digite a operação: + ou -   e pressione Enter");
+                operacao = Convert.ToChar(Console.ReadLine());
+
+                // Pede ao usuario que digite os minutos
+                Console.WriteLine("Digite os minutos e pressione Enter");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                int dia = Convert.ToInt32(data.Substring(0, 2));
+                int mes = Convert.ToInt32(data.Substring(3, 2));
+                int ano = Convert.ToInt32(data.Substring(6, 4));
+                int horas = Convert.ToInt32(data.Substring(11, 2));
+                int minutos = Convert.ToInt32(data.Substring(14, 2));
+
+                // Calcula o resultado do método e mostra na tela
+                FuncaoData dataCalculada = new FuncaoData();
+
+                string resultado = dataCalculada.CalcularData(data, operacao, valor);
+                Console.WriteLine("O resultado é " + resultado);
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
